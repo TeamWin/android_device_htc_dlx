accbdcfa7289bced70acb65a0ca59ab10129f514
Dan Pasanen <dan.pasanen@gmail.com>
2013-08-14 20:44:33 -0500

    fix barrier
    Change-Id: I93d44b228abdbfb37671268644e2f86b417f3660

--- Android.mk
+++ Android.mk
@@ -14,8 +14,9 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := $(call my-dir)
-
 ifeq ($(TARGET_DEVICE),dlx)
-    include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+LOCAL_PATH := $(call my-dir)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
 endif
