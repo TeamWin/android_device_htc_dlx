5a1db9af49fb7e1c2cca08a9e2c29f8956a5a178
Matt Mower <mowerm@gmail.com>
2014-08-31 13:30:18 -0500

    Extend SEPolicy
    Change-Id: I433788c441341062f3b61b5409716b8c9356d962

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,31 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
 
+# SELinux
+BOARD_SEPOLICY_DIRS += device/htc/dlx/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    app.te \
+    bluetooth.te \
+    device.te \
+    domain.te \
+    drmserver.te \
+    file_contexts \
+    file.te \
+    hci_init.te \
+    healthd.te \
+    init_shell.te \
+    init.te \
+    keystore.te \
+    kickstart.te \
+    mediaserver.te \
+    rild.te \
+    surfaceflinger.te \
+    system.te \
+    ueventd.te \
+    wpa_socket.te \
+    wpa.te
+
 # TWRP
 DEVICE_RESOLUTION := 1080x1920
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
